a9dd9a212bf87f1f31b18b75e6727b44e9337f5a
Camfabjeh <camille.fabry@yahoo.com>
2023-06-08 10:05:24 +0200

    mise en place routes works en mode initial + debug DB

---
+++ backend/src/controllers/workControllers.js
@@ -0,0 +1,91 @@
+const models = require("../models");
+
+const browse = (req, res) => {
+  models.work
+    .findAll()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const read = (req, res) => {
+  models.work
+    .find(req.params.id)
+    .then(([rows]) => {
+      if (rows[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const edit = (req, res) => {
+  const work = req.body;
+
+  // TODO validations (length, format...)
+
+  work.id = parseInt(req.params.id, 10);
+
+  models.work
+    .update(work)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const work = req.body;
+
+  // TODO validations (length, format...)
+
+  models.work
+    .insert(work)
+    .then(([result]) => {
+      res.location(`/works/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  models.work
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browse,
+  read,
+  edit,
+  add,
+  destroy,
+};
---
+++ backend/src/models/WorkManager.js
@@ -0,0 +1,27 @@
+const AbstractManager = require("./AbstractManager");
+
+class WorkManager extends AbstractManager {
+  constructor() {
+    super({ table: "works" });
+  }
+
+  find(id) {
+    return this.database.query(`select * from  ${this.table} where id = ?`, [
+      id,
+    ]);
+  }
+
+  findAll() {
+    return this.database.query(`select * from  ${this.table}`);
+  }
+
+  delete(id) {
+    return this.database.query(`delete from ${this.table} where id = ?`, [id]);
+  }
+
+  setDatabase(database) {
+    this.database = database;
+  }
+}
+
+module.exports = WorkManager;
