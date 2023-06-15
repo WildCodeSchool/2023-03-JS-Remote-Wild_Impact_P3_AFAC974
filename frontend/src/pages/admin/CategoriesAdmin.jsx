d97be6b6ad514fff3889a37d8c7613cd77b69b71
Bnt-dev <bberanger.pro@gmail.com>
2023-06-15 16:39:08 +0200

    récupération des datas et contrôle des champs via state

--- frontend/src/pages/admin/CategoriesAdmin.jsx
+++ frontend/src/pages/admin/CategoriesAdmin.jsx
@@ -1,29 +1,63 @@
 import React, { useState } from "react";
-import connexion from "../../services/connexion";
+// import connexion from "../../services/connexion";
 
 function CategoriesAdmin() {
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState({
+    name: "",
+    sousCat: "",
+  });
 
-  const handleCategory = async (event) => {
-    event.preventDefault();
-    const result = await connexion.post("/categories", { category });
-    console.info(result);
+  const handleCategory = (name, value) => {
+    setCategory({ ...category, [name]: value });
   };
+
+  // const handleCategory = async (event) => {
+  //   event.preventDefault();
+  //   const result = await connexion.post("/categories", { category });
+  //   console.info(result);
+  // };
+
+  // const deleteCategory = async (event) => {
+  //   event.preventDefault();
+  // fetch(`${import.meta.env.VITE_BACKEND_URL}/categories/${category.id}`, {
+  //   method: "DELETE",    // })
+  //   .then((res) => res.json())
+  //   .then((json) => console.info(json))
+  //   .catch((err) => console.error(err));
 
   return (
     <div>
-      <form onSubmit={(event) => handleCategory(event)}>
+      <form>
+        {/* onSubmit={(event) => handleCategory(event)} */}
         <label>
           Nom de la catégorie
           <input
             type="text"
             minLength={4}
-            value={category}
-            onChange={(event) => setCategory(event.target.value)}
+            name="name"
+            onChange={(event) =>
+              handleCategory(event.target.name, event.target.value)
+            }
+            value={category.name}
+          />
+        </label>
+        <label>
+          Nom de la sous-catégorie
+          <input
+            type="text"
+            minLength={4}
+            name="sousCat"
+            onChange={(event) =>
+              handleCategory(event.target.name, event.target.value)
+            }
+            value={category.sousCat}
           />
         </label>
         <button type="submit">Ajouter</button>
       </form>
+      {/* <button type="button" onClick={(event) => deleteCategory(event)}>
+        Supprimer
+      </button> */}
     </div>
   );
 }
