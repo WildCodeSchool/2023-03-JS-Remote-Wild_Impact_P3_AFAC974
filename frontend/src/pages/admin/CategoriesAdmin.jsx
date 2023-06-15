f64aeb89862d4d36c318188f6f5a046e9f32bffa
Bnt-dev <bberanger.pro@gmail.com>
2023-06-15 17:34:18 +0200

    frontend route post màj bdd

--- frontend/src/pages/admin/CategoriesAdmin.jsx
+++ frontend/src/pages/admin/CategoriesAdmin.jsx
@@ -4,11 +4,25 @@ import React, { useState } from "react";
 function CategoriesAdmin() {
   const [category, setCategory] = useState({
     name: "",
-    sousCat: "",
   });
 
   const handleCategory = (name, value) => {
     setCategory({ ...category, [name]: value });
+  };
+
+  const postCategory = (event) => {
+    event.preventDefault();
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/categories`, {
+      method: "POST",
+      body: JSON.stringify(category),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => console.info(json))
+      .catch((err) => console.error(err));
   };
 
   // const handleCategory = async (event) => {
@@ -27,9 +41,8 @@ function CategoriesAdmin() {
 
   return (
     <div>
-      <form>
-        {/* onSubmit={(event) => handleCategory(event)} */}
-        <label>
+      <form onSubmit={(event) => postCategory(event)}>
+        <label htmlFor="">
           Nom de la catégorie
           <input
             type="text"
@@ -39,18 +52,6 @@ function CategoriesAdmin() {
               handleCategory(event.target.name, event.target.value)
             }
             value={category.name}
-          />
-        </label>
-        <label>
-          Nom de la sous-catégorie
-          <input
-            type="text"
-            minLength={4}
-            name="sousCat"
-            onChange={(event) =>
-              handleCategory(event.target.name, event.target.value)
-            }
-            value={category.sousCat}
           />
         </label>
         <button type="submit">Ajouter</button>
