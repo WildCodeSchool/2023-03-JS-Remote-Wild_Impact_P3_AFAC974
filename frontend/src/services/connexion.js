f981dad1de6645b63aec79d9202882f85187ef51
Bnt-dev <bberanger.pro@gmail.com>
2023-06-21 09:07:15 +0200

    routes post catégories

--- frontend/src/services/connexion.js
+++ frontend/src/services/connexion.js
@@ -1,5 +1,5 @@
 const postData = (path, data) => {
-  console.log(data);
+  // console.log(data);
   return fetch(`${import.meta.env.VITE_BACKEND_URL}${path}`, {
     method: "POST",
     body: JSON.stringify(data),
@@ -12,8 +12,17 @@ const postData = (path, data) => {
     .catch((err) => console.error(err));
 };
 
+const deleteData = (path) => {
+  return fetch(`${import.meta.env.VITE_BACKEND_URL}${path}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
+
 const connexion = {
   post: (path, data) => postData(path, data),
+  delete: (path, id) => deleteData(path, id),
 };
 
 export default connexion;
