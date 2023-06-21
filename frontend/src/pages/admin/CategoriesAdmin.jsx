409cc2f20ad33dd6317c9475b3d5fa0b1a923582
Bnt-dev <bberanger.pro@gmail.com>
2023-06-21 14:39:56 +0200

    Mise en place route delete

--- frontend/src/pages/admin/CategoriesAdmin.jsx
+++ frontend/src/pages/admin/CategoriesAdmin.jsx
@@ -8,11 +8,15 @@ function CategoriesAdmin() {
 
   const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
+  const getCategories = () => {
     fetch(`${import.meta.env.VITE_BACKEND_URL}/categories`)
       .then((res) => res.json())
       .then((data) => setCategories(data))
       .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    getCategories();
   }, []);
 
   const handleCategory = (name, value) => {
@@ -30,7 +34,10 @@ function CategoriesAdmin() {
       },
     })
       .then((res) => res.json())
-      .then((json) => setCategory(json))
+      .then((json) => {
+        setCategory(json);
+        getCategories();
+      })
       .catch((err) => console.error(err));
   };
 
@@ -52,7 +59,12 @@ function CategoriesAdmin() {
           className="block mb-2 text-sm font-medium text-gray dark:text-white"
         >
           Selectionner une catégorie
-          <select className="bg-gray border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <select
+            onChange={(e) =>
+              setCategory(categories.find((cat) => cat.id === +e.target.value))
+            }
+            className="bg-gray border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
             <option value="">Choisir la catégorie</option>
             {categories.map((cat) => (
               <option key={cat.id} value={cat.id}>
