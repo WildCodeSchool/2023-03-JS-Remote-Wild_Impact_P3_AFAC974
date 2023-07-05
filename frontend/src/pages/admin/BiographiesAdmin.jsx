c54b54f7ea2d6024fccf288e5a2d96369c96afc6
Maillardjay <jay_10@hotmail.fr>
2023-07-05 11:03:13 +0200

    mise en place de la page biographie avec Get des données

--- frontend/src/pages/admin/BiographiesAdmin.jsx
+++ frontend/src/pages/admin/BiographiesAdmin.jsx
@@ -2,171 +2,269 @@ import React, { useState, useEffect } from "react";
 import connexion from "../../services/connexion";
 
 function BiographiesAdmin() {
-  const [work, setWork] = useState({
-    reference: "",
-    title: "",
-    summary_title: "",
-    date: "",
-    summary1: "",
-    summary2: "",
-    summary3: "",
-    summary4: "",
-    techniques_id: "",
-    format: "",
-    categories_id: "",
-    image_src: "",
-    image_alt: "",
-  });
-
-  const handleWork = (name, value) => {
-    setWork({ ...work, [name]: value });
+  const biographyModel = {
+    name: "",
+    title1: "",
+    image1_src: "",
+    image1_alt: "",
+    text1: "",
+    title2: "",
+    image2_src: "",
+    image2_alt: "",
+    text2: "",
+    title3: "",
+    image3_src: "",
+    image3_alt: "",
+    text3: "",
   };
 
-  const postWork = async (event) => {
-    event.preventDefault();
+  const [biography, setBiography] = useState({ biographyModel });
+  const [biographies, setBiographies] = useState([]);
+
+  const refreshBiography = (id) => {
+    if (id === "") {
+      setBiography(biographyModel);
+    } else {
+      setBiography(biographies.find((bio) => bio.id === +id));
+    }
+  };
+
+  const getBiographies = async () => {
     try {
-      const works = await connexion.post("/works", work);
-      setWork(works);
+      const bio = await connexion.get("/biographies");
+      setBiographies(bio);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const [techniques, setTechniques] = useState([]);
-
-  const getTechniques = () => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/techniques`)
-      .then((res) => res.json())
-      .then((data) => setTechniques(data))
-      .catch((err) => console.error(err));
-  };
-
   useEffect(() => {
-    getTechniques();
+    getBiographies();
   }, []);
 
-  const [categories, setCategories] = useState([]);
-
-  const getCategories = () => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/categories`)
-      .then((res) => res.json())
-      .then((data) => setCategories(data))
-      .catch((err) => console.error(err));
+  const handleBiography = (name, value) => {
+    setBiography({ ...biography, [name]: value });
   };
-
-  useEffect(() => {
-    getCategories();
-  }, []);
 
   return (
     <div className="flex-1">
       <h1 className="text-right pr-5 pt-5 text-2xl font-bold">Page Admin</h1>
-      <h2 className="text-xl font-bold p-4 pb-10">Gestion des oeuvres</h2>
-      <form
-        className="flex justify-around"
-        onSubmit={(event) => postWork(event)}
-      >
-        <div className="w-80">
-          <div>
-            <label className="flex flex-col font-semibold">
-              Référence
-              <input
-                className="border border-black h-7"
-                type="text"
-                required
-                placeholder="Tapez ici la référence de l'oeuvre"
-                minLength={5}
-                maxLength={12}
-                name="reference"
-                onChange={(event) =>
-                  handleWork(event.target.name, event.target.value)
-                }
-                value={work.reference}
-              />
-            </label>
-          </div>
+      <h2 className="text-xl font-bold p-4 pb-10">Gestion des Biographies</h2>
+
+      <form>
+        <label
+          htmlFor="Select biographies"
+          className="flex flex-col font-semibold w-80"
+        >
+          <select
+            onChange={(e) => refreshBiography(e.target.value)}
+            value={biography.id}
+            className="border border-black h-7 mt-10 text-black"
+          >
+            <option value="">Choisir la biographie</option>
+            {biographies.map((bio) => (
+              <option key={bio.id} value={bio.id}>
+                {bio.name}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label
+          htmlFor="Write biographie"
+          className="flex flex-col font-semibold w-80"
+        >
+          <input
+            required
+            type="text"
+            className="border border-black h-7 mt-10"
+            placeholder="Tapez ici le nom de la catégorie"
+            name="name"
+            value={biography.name}
+            onChange={(e) => handleBiography(e.target.name, e.target.value)}
+          />
+        </label>
+
+        <div>
+          <label className="flex flex-col font-semibold pb-5">
+            Texte alternatif de l'image part.1
+            <input
+              className="border border-black h-7"
+              type="text"
+              required
+              placeholder="Décrivez l'oeuvre en quelques mots (à destination des personnes déficientes visuelles"
+              name="image_alt"
+              onChange={(event) =>
+                handleBiography(event.target.name, event.target.value)
+              }
+              value={biography.image1_alt}
+            />
+          </label>
+        </div>
+        <div>
+          <label className="flex flex-col font-semibold pb-5">
+            Image part.1
+            <input
+              className="border border-black h-7"
+              type="text"
+              required
+              placeholder="Image"
+              name="image_src"
+              onChange={(event) =>
+                handleBiography(event.target.name, event.target.value)
+              }
+              value={biography.image1_src}
+            />
+          </label>
+        </div>
+        <div className="pt-5">
+          <label className="flex flex-col font-semibold pb-5">
+            Titre biographie part.1
+            <input
+              className="border border-black h-7"
+              type="text"
+              required
+              placeholder="Titre de la biographie"
+              minLength={5}
+              maxLength={255}
+              name="title"
+              onChange={(event) =>
+                handleBiography(event.target.name, event.target.value)
+              }
+              value={biography.title1}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label className="flex flex-col font-semibold pb-5">
+            Commentaire 1
+            <textarea
+              className="border border-black"
+              required
+              placeholder="Description"
+              minLength={50}
+              name="summary1"
+              onChange={(event) =>
+                handleBiography(event.target.name, event.target.value)
+              }
+              value={biography.text1}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label className="flex flex-col font-semibold pb-5">
+            Texte alternatif de l'image part.2
+            <input
+              className="border border-black h-7"
+              type="text"
+              required
+              placeholder="Décrivez l'oeuvre en quelques mots (à destination des personnes déficientes visuelles"
+              name="image_alt"
+              onChange={(event) =>
+                handleBiography(event.target.name, event.target.value)
+              }
+              value={biography.image2_alt}
+            />
+          </label>
+        </div>
+        <div>
+          <label className="flex flex-col font-semibold pb-5">
+            Image part.2
+            <input
+              className="border border-black h-7"
+              type="text"
+              required
+              placeholder="Image"
+              name="image_src"
+              onChange={(event) =>
+                handleBiography(event.target.name, event.target.value)
+              }
+              value={biography.image2_src}
+            />
+          </label>
+        </div>
+        <div className="pt-5">
+          <label className="flex flex-col font-semibold pb-5">
+            Titre biographie part.2
+            <input
+              className="border border-black h-7"
+              type="text"
+              required
+              placeholder="Titre de la biographie"
+              minLength={5}
+              maxLength={255}
+              name="title"
+              onChange={(event) =>
+                handleBiography(event.target.name, event.target.value)
+              }
+              value={biography.title2}
+            />
+          </label>
+        </div>
+        <div>
+          <label className="flex flex-col font-semibold pb-5">
+            Commentaire 2
+            <textarea
+              className="border border-black"
+              placeholder="Description"
+              minLength={50}
+              name="summary2"
+              onChange={(event) =>
+                handleBiography(event.target.name, event.target.value)
+              }
+              value={biography.text2}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label className="flex flex-col font-semibold pb-5">
+            Texte alternatif de l'image 3
+            <input
+              className="border border-black h-7"
+              type="text"
+              required
+              placeholder="Décrivez l'oeuvre en quelques mots (à destination des personnes déficientes visuelles"
+              name="image_alt"
+              onChange={(event) =>
+                handleBiography(event.target.name, event.target.value)
+              }
+              value={biography.image3_alt}
+            />
+          </label>
+        </div>
+        <div>
+          <label className="flex flex-col font-semibold pb-5">
+            Image part.3
+            <input
+              className="border border-black h-7"
+              type="text"
+              required
+              placeholder="Image"
+              name="image_src"
+              onChange={(event) =>
+                handleBiography(event.target.name, event.target.value)
+              }
+              value={biography.image3_src}
+            />
+          </label>
+
           <div className="pt-5">
             <label className="flex flex-col font-semibold pb-5">
-              Titre de l'oeuvre
+              Titre biographie part.3
               <input
                 className="border border-black h-7"
                 type="text"
                 required
-                placeholder="Titre de l'oeuvre"
+                placeholder="Titre de la biographie"
                 minLength={5}
                 maxLength={255}
                 name="title"
                 onChange={(event) =>
-                  handleWork(event.target.name, event.target.value)
+                  handleBiography(event.target.name, event.target.value)
                 }
-                value={work.title}
-              />
-            </label>
-          </div>
-          <div>
-            <label className="flex flex-col font-semibold pb-5">
-              Titre Résumé
-              <input
-                className="border border-black h-7"
-                type="text"
-                required
-                placeholder="Titre Résumé"
-                minLength={10}
-                maxLength={255}
-                name="summary_title"
-                onChange={(event) =>
-                  handleWork(event.target.name, event.target.value)
-                }
-                value={work.summary_title}
-              />
-            </label>
-          </div>
-          <div>
-            <label className="flex flex-col font-semibold  pb-5 ">
-              Année de réalisation
-              <input
-                className="border border-black h-7"
-                type="text"
-                required
-                placeholder="Année de réalisation"
-                minLength={4}
-                maxLength={50}
-                name="date"
-                onChange={(event) =>
-                  handleWork(event.target.name, event.target.value)
-                }
-                value={work.date}
-              />
-            </label>
-          </div>
-          <div>
-            <label className="flex flex-col font-semibold pb-5">
-              Commentaire 1
-              <textarea
-                className="border border-black"
-                required
-                placeholder="Description"
-                minLength={50}
-                name="summary1"
-                onChange={(event) =>
-                  handleWork(event.target.name, event.target.value)
-                }
-                value={work.summary1}
-              />
-            </label>
-          </div>
-          <div>
-            <label className="flex flex-col font-semibold pb-5">
-              Commentaire 2
-              <textarea
-                className="border border-black"
-                placeholder="Description"
-                minLength={50}
-                name="summary2"
-                onChange={(event) =>
-                  handleWork(event.target.name, event.target.value)
-                }
-                value={work.summary2}
+                value={biography.title3}
               />
             </label>
           </div>
@@ -179,135 +277,22 @@ function BiographiesAdmin() {
                 minLength={50}
                 name="summary3"
                 onChange={(event) =>
-                  handleWork(event.target.name, event.target.value)
+                  handleBiography(event.target.name, event.target.value)
                 }
-                value={work.summary3}
+                value={biography.text3}
               />
             </label>
           </div>
-          <div>
-            <label className="flex flex-col font-semibold pb-5">
-              Commentaire 4
-              <textarea
-                className="border border-black"
-                placeholder="Description"
-                minLength={50}
-                name="summary4"
-                onChange={(event) =>
-                  handleWork(event.target.name, event.target.value)
-                }
-                value={work.summary4}
-              />
-            </label>
-          </div>
-        </div>
-        <div>
-          <div className="pt-50">
-            <div>
-              <label className="flex flex-col font-semibold pb-5">
-                Technique
-                <select
-                  className="border border-black h-7"
-                  name="techniques_id"
-                  type="text"
-                  onChange={(event) =>
-                    handleWork(event.target.name, +event.target.value)
-                  }
-                >
-                  <option value="">
-                    Choisissez la technique à associer avec l'oeuvre
-                  </option>
-                  {techniques.map((tech) => (
-                    <option key={tech.id} value={tech.id}>
-                      {tech.name}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </div>
-            <div>
-              <label className="flex flex-col font-semibold pb-5">
-                Dimensions
-                <input
-                  className="border border-black h-7"
-                  type="text"
-                  required
-                  placeholder="Notez ici les dimensions de l'oeuvre, en cm"
-                  minLength={6}
-                  maxLength={50}
-                  name="format"
-                  onChange={(event) =>
-                    handleWork(event.target.name, event.target.value)
-                  }
-                  value={work.format}
-                />
-              </label>
-            </div>
-            <div>
-              <label className="flex flex-col font-semibold pb-5">
-                Catégorie
-                <select
-                  className="border border-black h-7 "
-                  name="categories_id"
-                  type="text"
-                  onChange={(event) =>
-                    handleWork(event.target.name, +event.target.value)
-                  }
-                >
-                  <option value="">
-                    Choisissez la catégorie à associer avec l'oeuvre
-                  </option>
-                  {categories.map((cat) => (
-                    <option key={cat.id} value={cat.id}>
-                      {cat.name}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </div>
-            <div>
-              <label className="flex flex-col font-semibold pb-5">
-                Texte alternatif de l'image
-                <input
-                  className="border border-black h-7"
-                  type="text"
-                  required
-                  placeholder="Décrivez l'oeuvre en quelques mots (à destination des personnes déficientes visuelles"
-                  name="image_alt"
-                  onChange={(event) =>
-                    handleWork(event.target.name, event.target.value)
-                  }
-                  value={work.image_alt}
-                />
-              </label>
-            </div>
-            <div>
-              <label className="flex flex-col font-semibold pb-5">
-                Image
-                <input
-                  className="border border-black h-7"
-                  type="file"
-                  required
-                  placeholder="Image"
-                  name="image_src"
-                  onChange={(event) =>
-                    handleWork(event.target.name, event.target.value)
-                  }
-                  value={work.image_src}
-                />
-              </label>
-              <div className="flex justify-end pt-60 pb-5 pr-10 gap-10">
-                <button type="submit" className="bg-black text-white py-2 px-4">
-                  Ajouter
-                </button>
-                <button type="button" className="bg-black text-white py-2 px-4">
-                  Modifier
-                </button>
-                <button type="button" className="bg-black text-white py-2 px-4">
-                  Supprimer
-                </button>
-              </div>
-            </div>
+          <div className="flex justify-end pt-60 pb-5 pr-10 gap-10">
+            <button type="submit" className="bg-black text-white py-2 px-4">
+              Ajouter
+            </button>
+            <button type="button" className="bg-black text-white py-2 px-4">
+              Modifier
+            </button>
+            <button type="button" className="bg-black text-white py-2 px-4">
+              Supprimer
+            </button>
           </div>
         </div>
       </form>
