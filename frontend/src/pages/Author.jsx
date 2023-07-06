04592096c21afb66ee424b7bacd0fe7c20faf34d
Maillardjay <jay_10@hotmail.fr>
2023-07-06 21:27:46 +0200

    préparation de de la page Biographie

--- frontend/src/pages/Author.jsx
+++ frontend/src/pages/Author.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Author() {
-  return <div>Author</div>;
+  return (
+    <div className="bg-black h-screen">
+      <h1 className="text-right text-white underline-offset-8 font-bold text-2xl">
+        BIOGRAPHIE
+      </h1>
+      <div className="border-t-4 border-pink h-100 w-full"> </div>
+      <div className="name= Paragraphes flex text-white ">
+        <p>img3</p>
+        <h3>titre 1</h3>
+        <p> text 1</p>
+      </div>
+      <div className="name= Paragraphes flex text-white ">
+        <p>img3</p>
+        <h3>titre 2</h3>
+        <p> text 2</p>
+      </div>
+      <div className="name= Paragraphes flex text-white ">
+        <p>img3</p>
+        <h3>titre 3</h3>
+        <p> text 3</p>
+      </div>
+    </div>
+  );
 }
 
 export default Author;
