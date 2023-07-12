d1c9ba1d69c6f0eb327f639d36df2d87f59d8b21
NicolasPanis <nicolaspanis12@gmail.com>
2023-07-12 11:32:55 +0200

    Placement Navbar

--- frontend/src/components/NavBarUser.jsx
+++ frontend/src/components/NavBarUser.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import user from "../assets/user.svg";
+import logo from "../assets/logo.png";
 
 function NavBarUser() {
   return (
-    <div>
-      <Link to="/">HomePage</Link>
-      <Link to="/gallery">Gallery</Link>
-      <Link to="/author">Author</Link>
-      <Link to="/about">About</Link>
-      <Link to="/favourites">Your favourites</Link>
+    <div className="pt-2 pl-10">
+      <ul className="flex justify-between items-center">
+        <img className="h-20" src={logo} alt="logo" />
+        <div className="flex gap-5">
+          <Link to="/">Accueil</Link>
+          <Link to="/gallery">Gallerie</Link>
+          <Link to="/author">Biographie</Link>
+          <Link to="/about">A propos</Link>
+        </div>
+        <div className="flex items-center pr-10">
+          <img className="h-6 pr-4" src={user} alt="user" />
+          <Link className="pt-1" to="/favourites">
+            Connexion
+          </Link>
+        </div>
+      </ul>
     </div>
   );
 }
