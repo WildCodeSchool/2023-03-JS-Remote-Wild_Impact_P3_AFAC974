a1442d9f16c97a54c65758ad5c4e4938ebda1e31
Maillardjay <jay_10@hotmail.fr>
2023-07-12 11:47:11 +0200

    mise en place de l'import du Footer dans le UserLayout

--- frontend/src/pages/layout/UserLayout.jsx
+++ frontend/src/pages/layout/UserLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBarUser from "../../components/NavBarUser";
+import Footer from "../../components/Footer";
 
 function UserLayout() {
   return (
     <div>
       <NavBarUser />
       <Outlet />
+      <Footer />
     </div>
   );
 }
