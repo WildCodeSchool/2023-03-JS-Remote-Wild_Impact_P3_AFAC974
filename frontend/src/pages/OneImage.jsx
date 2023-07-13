5f0362c6d76ca70309c8ac1e2b7c3245fb68bb43
Camfabjeh <camille.fabry@yahoo.com>
2023-07-13 10:01:54 +0200

    mise à jour ergonomie page OneImage + ajout inner join categories + techniques dans le WorkManager en back

--- frontend/src/pages/OneImage.jsx
+++ frontend/src/pages/OneImage.jsx
@@ -27,47 +27,41 @@ function OneImage() {
         {oneImage.title}
       </h1>
       <div className="border-t-2 border-pink h-100 ml-20 mr-5 mb-10" />
-      <div className="flex flex-col text-white ">
-        <div className="flex ml-20">
-          <ImageCard
-            cls="max-h-sm max-w-sm"
-            src={oneImage.image_src}
-            alt={oneImage.image_alt}
-          />
-          <div className="flex-col">
-            <div className="flex pb-5">
-              <img className="w-11 h-10 mr-2 ml-3" src={hexa} alt="logo" />
-              <h3 className="mt-2 font-bold text-xl">Description</h3>
-            </div>
-            <p className="text-left text-sm ml-5 mr-5">{oneImage.summary1}</p>
-            <br />
-            <p className="text-left text-sm ml-5 mr-5">{oneImage.summary2}</p>
-            <br />
-            <p className="text-left text-sm ml-5 mr-5">{oneImage.summary3}</p>
-            <br />
-            <p className="text-left text-sm ml-5 mr-5">{oneImage.summary4}</p>
-            <div className="border-t-2 border-pink h-100 m-5" />
-            <p className="text-left text-sm ml-5 mr-5">
-              Technique : {oneImage.techniquesId}
-            </p>
-            <div className="border-t-2 border-pink h-100 m-5" />
-            <p className="text-left text-sm ml-5 mr-5">
-              Catégorie : {oneImage.categoriesId}
-            </p>
-            <div className="border-t-2 border-pink h-100 m-5" />
-            <p className="text-left text-sm ml-5 mr-5">
-              Dimensions : {oneImage.format}
-            </p>
-            <div className="border-t-2 border-pink h-100 m-5" />
-            <p className="text-left text-sm ml-5 mr-5">
-              Date : {oneImage.date}
-            </p>
-            <div className="border-t-2 border-pink h-100 m-5" />
-          </div>
-        </div>
-        <h1 className="text-left text-white underline-offset-8 font-bold text-2xl pt-5 mr-5 ml-20 mt-10 mb-20">
-          OEUVRES SIMILAIRES
-        </h1>
+      <div className="flex justify-center">
+        <ImageCard
+          cls="max-h-sm max-w-sm content-center"
+          src={oneImage.image_src}
+          alt={oneImage.image_alt}
+        />
+      </div>
+      <div className="flex ml-10 pt-10 pb-5 text-white">
+        <img className="w-11 h-10 mr-2 ml-3" src={hexa} alt="logo" />
+        <h3 className="mt-2 font-bold text-xl">Description</h3>
+      </div>
+      <div className="text-white ml-10">
+        <p className="text-left text-sm ml-5 mr-5 pb-5 ">{oneImage.summary1}</p>
+        <p className="text-left text-sm ml-5 mr-5 pb-5 ">{oneImage.summary2}</p>
+        <p className="text-left text-sm ml-5 mr-5 pb-5 ">{oneImage.summary3}</p>
+        <p className="text-left text-sm ml-5 mr-5 pb-5 ">{oneImage.summary4}</p>
+        <div className="border-t-2 border-pink h-100 m-5" />
+        <p className="text-left text-sm ml-5 mr-5">
+          Technique : {oneImage.techniquesId}
+        </p>
+        <div className="border-t-2 border-pink h-100 m-5" />
+        <p className="text-left text-sm ml-5 mr-5">
+          Catégorie : {oneImage.categoriesId}
+        </p>
+        <div className="border-t-2 border-pink h-100 m-5" />
+        <p className="text-left text-sm ml-5 mr-5">
+          Dimensions : {oneImage.format}
+        </p>
+        <div className="border-t-2 border-pink h-100 m-5" />
+        <p className="text-left text-sm ml-5 mr-5">Date : {oneImage.date}</p>
+        <div className="border-t-2 border-pink h-100 m-5" />
+      </div>
+      <div className="flex ml-10 pt-10 pb-5 text-white">
+        <img className="w-11 h-10 mr-2 ml-3" src={hexa} alt="logo" />
+        <h3 className="mt-2 font-bold text-xl">Oeuvres similaires</h3>
       </div>
     </div>
   );
