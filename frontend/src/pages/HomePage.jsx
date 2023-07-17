0993b7a62e20ba6023b4fea968550ef88abf1090
Camfabjeh <camille.fabry@yahoo.com>
2023-07-17 12:16:38 +0200

    mise en place du modal protection des images depuis le clic sur entrer, sur la page d'accueil

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Modal from "react-modal";
+import hexa from "../assets/hexa.png";
+
 import bouton from "../assets/bouton.png";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    border: "3px solid black",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
 function HomePage() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+    navigate("/gallery");
+  }
+
   return (
     <div className="bg-cover h-screen w-screen bg-[url('src/assets/background.jpg')] flex flex-col justify-center items-center">
       <h2 className="text-white text-2xl text-center pr-[450px] pb-2">
@@ -15,31 +41,67 @@ function HomePage() {
         De Trévise
       </h1>
       <div className="">
-        <h3 className="relative z-20 bg-black bg-opacity-60 w-[10rem] h-[3rem] border-solid border-white border-4 text-xl text-white text-center mt-12 ml-[3rem] mb-[-1rem] pt-2 pb-2 ">
+        <h3 className="relative z-3 bg-black bg-opacity-60 w-[10rem] h-[3rem] border-solid border-white border-4 text-xl text-white text-center mt-12 ml-[3rem] mb-[-1rem] pt-2 pb-2 ">
           Le Concept
         </h3>
         <p className="bg-black w-[500px] h-[18rem] bg-opacity-60 text-white p-10 ml-[10rem]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in
-          sagittis libero. Nullam porttitor consectetur enim molestie rhoncus.
-          Morbi lacinia dolor non magna aliquet dapibus. Morbi a venenatis ante.
-          Ut imperdiet consectetur sem et dapibus. Nulla blandit condimentum
-          rutrum. Cras hendrerit, lectus eu dapibus sagittis, augue ligula
-          eleifend justo, vitaluctus odio sem sed dui.
+          L'exposition a pour but de présenter des œuvres de M. Mortier de
+          Trévise réalisées durant la décennie 1861-1871 durant son séjour à
+          l'île de La Réunion. <br />
+          Elles illustrent la vie quotidienne des travailleurs dans les grandes
+          plantations du Sud. La vie à cette époque n'était pas aussi tendre
+          qu'aujourd'hui. <br />
+          Malgré cela, l'artiste nous propose une vision apaisée de la situation
+          sans gommer la dureté de la vie sur cette île tropicale.
         </p>
         <div className="relative ml-[50px]">
           <img
             className="h-[10rem] pl-[539px] mt-[-5rem]"
             src={bouton}
-            alt="logo_deconnexion"
+            alt="logo_entrer sur le site"
           />
-          <Link
-            to="/gallery"
+          <button
+            type="button"
+            onClick={openModal}
             className="absolute bottom-[60px] right-[20px] text-white text-3xl"
           >
-            ENTREZ
-          </Link>
+            ENTRER
+          </button>
         </div>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        style={customStyles}
+        contentLabel="pop-up protection des images"
+      >
+        <div className="flex flex-col text-center">
+          <h2>
+            Les représentations des oeuvres que vous allez consulter sur ce site
+            ne sont pas libres de droit. <br /> <br />
+            Elles sont la propriété de l'Iconothèque historique de l'océan
+            indien
+            <a
+              href="https://www.ihoi.org/app/photopro.sk/ihoi_icono/?"
+              target="_blank"
+              className="underline text-blue"
+              rel="noreferrer"
+            >
+              (en savoir plus)
+            </a>
+            . <br /> <br />
+            En cliquant sur "Entrer", vous reconnaissez avoir pris connaissance
+            de cette information.
+          </h2>
+          <button
+            onClick={closeModal}
+            type="button"
+            className="text-black text-3xl"
+          >
+            <img className="w-11 h-10 mr-2 ml-3" src={hexa} alt="logo" />
+            ENTRER
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 }
