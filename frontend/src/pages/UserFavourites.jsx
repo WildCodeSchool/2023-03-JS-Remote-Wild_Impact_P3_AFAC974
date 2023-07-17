5518cd9373745e41f6c6065adda19cad62c04a09
Bnt-dev <bberanger.pro@gmail.com>
2023-07-17 11:24:21 +0200

    Mise en place du template front pour les favoris

--- frontend/src/pages/UserFavourites.jsx
+++ frontend/src/pages/UserFavourites.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import hexa from "../assets/hexa.png";
 
 function UserFavourites() {
-  return <div>UserFavourites</div>;
+  return (
+    <div className="bg-black h-full pr-20">
+      <h1
+        className="text-right text-white underline-offset-8 font-bold text-2xl pt-5"
+        type="text"
+        value="text"
+      >
+        FAVORIS
+      </h1>
+      <div className="border-t-4 border-pink h-100 w-5/6 ml-20"> </div>
+      <div className="flex-col text-white ml-20 mt-10 pb-20">
+        <div className="flex flex-col">
+          <div className="flex">
+            <img className="w-10 h-10" src={hexa} alt="logo" />
+            <div className="flex flex-col">
+              <h2>Titre de l'oeuvre</h2>
+              <button type="button" className="bg-pink">
+                Supprimer
+              </button>
+            </div>
+          </div>
+
+          <div className="border-t-4 border-grey h-100 mt-4 mb-4"> </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default UserFavourites;
