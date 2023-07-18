60b2ef50ad3fcf4ace60841689cd7c4d097f0737
NicolasPanis <nicolaspanis12@gmail.com>
2023-07-18 15:34:32 +0200

    Fond blanc logo a propos

--- frontend/src/components/NavBarUser.jsx
+++ frontend/src/components/NavBarUser.jsx
@@ -6,10 +6,10 @@ import logo from "../assets/logo.png";
 
 function NavBarUser() {
   return (
-    <div className="mb-[-8rem] pl-10 text-white z-20 relative">
+    <div className="mb-[-8rem] pl-5 text-white z-20 relative pt-3">
       <ul className="flex justify-between items-center">
         <NavLink to="/">
-          <img className="h-20" src={logo} alt="logo" />
+          <img className="h-16" src={logo} alt="logo" />
         </NavLink>
 
         <div className="flex gap-5">
@@ -54,15 +54,14 @@ function NavBarUser() {
             A propos
           </NavLink>
         </div>
-        <div className="flex items-center pr-10 text-xs">
+        <div className="flex items-center pr-5 text-xs">
           <Link className="pt-1" to="/favourites">
             <img className="h-6 pr-4" src={user} alt="user" />
           </Link>
-          <Link className="pt-1" to="/auth/connexion">
+          <Link className="pt-1 text-[15px] mr-5" to="/auth/connexion">
             Connexion
           </Link>
-          <div className="flex items-center pr-10" />
-          <Link className="pt-1" to="/subscription">
+          <Link className="pt-1 text-[15px]" to="/subscription">
             Inscription
           </Link>
         </div>
