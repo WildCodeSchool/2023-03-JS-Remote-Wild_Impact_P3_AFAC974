ff9cee5ab4f769738adafafec613a3e8a6d13267
NicolasPanis <nicolaspanis12@gmail.com>
2023-07-18 15:48:45 +0200

    Bouton text white

--- frontend/src/components/GalleryImage.jsx
+++ frontend/src/components/GalleryImage.jsx
@@ -39,9 +39,9 @@ function GalleryImage() {
             <Link to={`/gallery/${work.id}`}>
               <button
                 type="button"
-                className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple to-pink group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+                className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple to-pink group-hover:from-purple group-hover:to-pink hover:text-white dark:text-white focus:ring-4"
               >
-                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                <span className="relative text-white px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   En savoir plus
                 </span>
               </button>
